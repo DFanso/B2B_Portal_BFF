d5c6c052ba48063d3e2408e20268d250383dd64a
Leo Gavin <leogavin123@outlook.com>
2023-12-30 19:07:27 +0530

    basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import * as Joi from 'joi';
+import { ulid } from 'ulid';
+import { ConfigModule } from '@nestjs/config';
+import { ClsModule } from 'nestjs-cls';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+    validationSchema: Joi.object({
+      AWS_REGION: Joi.string().required(),
+      COGNITO_CLIENT_ID: Joi.string().required(),
+      COGNITO_CLIENT_SECRET: Joi.string().required(),
+      USER_MICRO_API: Joi.string().required(),
+      PRODUCT_MICRO_API: Joi.string().required(),
+      ORDER_MICRO_API: Joi.string().required(),
+    }),
+  }),
+  ClsModule.forRoot({
+    middleware: {
+      mount: true,
+      setup: (cls, req, res) => {
+        const requestId = ulid();
+        cls.set('x-request-id', requestId);
+        res.setHeader('X-Request-ID', requestId);
+      },
+    },
+  }),AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useRequestLogging } from './request-logging';
+import { VersioningType } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    rawBody: true,
+    bodyParser: true,
+  });
+  useRequestLogging(app);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.use(
+    '/api',
+    basicAuth({
+      challenge: true,
+      users: {
+        admin: 'admin',
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('B2B Portal BFF')
+    .setDescription('B2B Portal BFF API')
+    .setVersion('1.0')
+    .addTag('B2B_Portal')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
+
